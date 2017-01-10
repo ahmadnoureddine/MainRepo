1a41614877d3d94fa159309148204b75fab3c31b
Ahmad Noureddine <Ahmadnnoureddine@gmail.com>
2017-01-10 22:39:46 +0200

    ATM Deposit Menu 1.0.0
    ATM Deposit Menu application. Allows user/agent to instantly deposit
    money to his or her account.

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DepositButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AmountToDepositTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DepositButton
+        '
+        Me.DepositButton.Location = New System.Drawing.Point(111, 74)
+        Me.DepositButton.Name = "DepositButton"
+        Me.DepositButton.Size = New System.Drawing.Size(103, 23)
+        Me.DepositButton.TabIndex = 0
+        Me.DepositButton.Text = "Deposit"
+        Me.DepositButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Amount To Deposit:"
+        '
+        'AmountToDepositTextBox
+        '
+        Me.AmountToDepositTextBox.AcceptsReturn = True
+        Me.AmountToDepositTextBox.Location = New System.Drawing.Point(111, 48)
+        Me.AmountToDepositTextBox.Name = "AmountToDepositTextBox"
+        Me.AmountToDepositTextBox.Size = New System.Drawing.Size(103, 20)
+        Me.AmountToDepositTextBox.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(82, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Deposit Menu - Arab Bank"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(91, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(297, 134)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.AmountToDepositTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DepositButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DepositButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AmountToDepositTextBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
